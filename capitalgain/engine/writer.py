87993f75c8ca9f0b97bb721ee57bdf28da33e646
Philip Mathieu <philip.eng.mathieu@gmail.com>
2015-02-07 21:09:23 -0500

    Revert "Fixed test script"
    This reverts commit 5864395032cc2ea71816f4472d3983981907aa7b.

--- capitalgain/engine/writer.py
+++ capitalgain/engine/writer.py
@@ -1,8 +1,6 @@
 #!usr/bin/env python
 import os
 from midiutil.MidiFile import MIDIFile
-from note_generator import phil_use_this
-
 theory = {'1':[1,1,3,5],'4':[4,4,6,8],'6':[6,6,8,10],'5':[5,5,7,9],'2':[2,2,4,6],'3':[3,3,5,7],'27':[2,2,4,6,8],'47':[4,4,6,8,10],'67':[6,6,8,10,12],'16':[1,3,5,8],'56':[5,7,9,12],'164':[1,5,8,10],'b7':[7,7,9,11],'57':[5,5,7,9,11],'37':[3,3,5,7,9],'5/5':[5,2,4.5,6],'17':[1,1,3,5,7],'464':[4,1,4,6],'664':[6,3,6,8],'564':[5,2,5,7],'46':[4,6,8,11],'57/6':[5,3,5.5,7,9],'66':[6,1,3,6],'36':[3,5,7,10]}
 MIDIOut = MIDIFile(4)
 
@@ -49,6 +47,6 @@ def write_chords(chords):
     return song
 
 if __name__ == '__main__':
-    test = phil_use_this()
-    print(write_chords(test))
-    write_midi(write_chords(test),'test')
+    #testchords =[{'name':'1','dur':3},{'name':'5','dur':1},{'name':'4','dur':2}]
+    print(write_chords(testchords))
+    write_midi(write_chords(testchords),'test')
