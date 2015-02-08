a180400d62d7447ce103f4e817913a59d5b64ba8
Philip Mathieu <philip.eng.mathieu@gmail.com>
2015-02-07 20:33:48 -0500

    continued debugging

--- capitalgain/engine/writer.py
+++ capitalgain/engine/writer.py
@@ -1,5 +1,5 @@
 #!usr/bin/env python
-import json
+import os
 from midiutil.MidiFile import MIDIFile
 theory = {'1':[1,1,3,5],'4':[4,4,6,8],'6':[6,6,8,10],'5':[5,5,7,9],'2':[2,2,4,6],'3':[3,3,5,7],'27':[2,2,4,6,8],'47':[4,4,6,8,10],'67':[6,6,8,10,12],'16':[1,3,5,8],'56':[5,7,9,12],'164':[1,5,8,10],'b7':[7,7,9,11],'57':[5,5,7,9,11],'37':[3,3,5,7,9],'5/5':[5,2,4.5,6],'17':[1,1,3,5,7],'464':[4,1,4,6],'664':[6,3,6,8],'564':[5,2,5,7],'46':[4,6,8,11],'57/6':[5,3,5.5,7,9],'66':[6,1,3,6],'36':[3,5,7,10]}
 MIDIOut = MIDIFile(4)
@@ -36,16 +36,17 @@ def write_chords(chords):
     for i in range(0,len(chords)):
         chord = chords[i]
         #get notes
-        notes = chord_to_notes(chord['name'],chord['dur'],pos)
+        #REAL ONE: notes = chord_to_notes(chord['name'],chord['dur'],pos)
+        notes = chord_to_notes(chord,1,pos) #TEST ONLY
         #add notes at pos
         song[0].append(notes[0])
         song[1].append(notes[1])
         song[2].append(notes[2])
         #pos += duration
-        pos += chord['dur']
+        pos += 1
     return song
 
 if __name__ == '__main__':
-    testchords =[{'name':'1','dur':3,'dur':2},{'name':'5','dur':1,'dur':3},{'name':'4','dur':2,'dur':1}]
+    #testchords =[{'name':'1','dur':3},{'name':'5','dur':1},{'name':'4','dur':2}]
     print(write_chords(testchords))
     write_midi(write_chords(testchords),'test')
