941789d6ff47272a3533ab0d2464bfea310d5620
Philip Mathieu <philip.eng.mathieu@gmail.com>
2015-02-08 00:20:09 -0500

    made easier re-keying

--- capitalgain/engine/writer.py
+++ capitalgain/engine/writer.py
@@ -2,8 +2,10 @@
 from pymongo import MongoClient
 from midiutil.MidiFile import *
 from note_generator import phil_use_this
+
 theory = {'1':[1,1,3,5],'4':[4,4,6,8],'6':[6,6,8,10],'5':[5,5,7,9],'2':[2,2,4,6],'3':[3,3,5,7],'27':[2,2,4,6,8],'47':[4,4,6,8,10],'67':[6,6,8,10,12],'16':[1,3,5,8],'56':[5,7,9,12],'164':[1,5,8,10],'b7':[7,7,9,11],'57':[5,5,7,9,11],'37':[3,3,5,7,9],'5/5':[5,2,4.5,6],'17':[1,1,3,5,7],'464':[4,1,4,6],'664':[6,3,6,8],'564':[5,2,5,7],'46':[4,6,8,11],'57/6':[5,3,5.5,7,9],'66':[6,1,3,6],'36':[3,5,7,10]}
 MIDIOut = MIDIFile(4)
+pitchup = 15
 
 def write_midi(song,stock):
     MIDIOut.addTrackName(0,0,'Bass')
@@ -22,11 +24,11 @@ def write_midi(song,stock):
 def pitch_to_notes(notes):
     notemap = {1:1,2:3,3:5,4:6,4.5:7,5:8,5.5:9,6:10,7:12,8:13,9:15,10:17,11:18,12:20,13:22,14:24,15:25,16:27,17:29,18:30,19:32,20:34,21:36,22:37,23:39,24:41}
     for note in notes[0]:
-        note['pitch'] = notemap[note['pitch']] + 24 #map to scale, repitch to C3
+        note['pitch'] = notemap[note['pitch']] + 2*pitchup#map to scale, repitch to C3
     for note in notes[1]:
-        note['pitch'] = notemap[note['pitch']] + 36 #map to scale, repitch to C3
+        note['pitch'] = notemap[note['pitch']] + 3*pitchup #map to scale, repitch to C3
     for note in notes[2]:
-        note['pitch'] = notemap[note['pitch']] + 48 #map to scale, repitch to C3
+        note['pitch'] = notemap[note['pitch']] + 4*pitchup #map to scale, repitch to C3
     return notes
 
 def chord_to_notes(chord,dur,posit,pos):
