6f806cee24e8b1d65c263756afd5b6cfbe9df50e
Philip Mathieu <philip.eng.mathieu@gmail.com>
2015-02-07 20:17:19 -0500

    Debugged

--- capitalgain/writer.py
+++ capitalgain/writer.py
@@ -1,6 +1,6 @@
 #!usr/bin/env python
 import json
-from midiutil import MidiFile
+from midiutil.MidiFile import MIDIFile
 theory = {'1':[1,1,3,5],'4':[4,4,6,8],'6':[6,6,8,10],'5':[5,5,7,9],'2':[2,2,4,6],'3':[3,3,5,7],'27':[2,2,4,6,8],'47':[4,4,6,8,10],'67':[6,6,8,10,12],'16':[1,3,5,8],'56':[5,7,9,12],'164':[1,5,8,10],'b7':[7,7,9,11],'57':[5,5,7,9,11],'37':[3,3,5,7,9],'5/5':[5,2,4.5,6],'17':[1,1,3,5,7],'464':[4,1,4,6],'664':[6,3,6,8],'564':[5,2,5,7],'46':[4,6,8,11],'57/6':[5,3,5.5,7,9],'66':[6,1,3,6],'36':[3,5,7,10]}
 MIDIOut = MIDIFile(4)
 
@@ -9,10 +9,10 @@ def write_midi(song,stock):
     MIDIOut.addTrackName(1,0,'Fifth')
     MIDIOut.addTrackName(2,0,'Chord')
     MIDIOut.addTrackName(3,0,'Melody')
-    for t in range(0,4):
+    for t in range(0,3):
         for chord in song[t]:
             for note in chord:
-                MIDIOut.addNote(t,1,note('pitch'),note('time'),note('dur'),note('vel'))
+                MIDIOut.addNote(t,1,note['pitch']+36,note['time'],note['dur'],note['vel'])
     binfile = open(stock+'.mid', 'wb')
     MIDIOut.writeFile(binfile)
     binfile.close()
