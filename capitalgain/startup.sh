a84b96fcd34041513c093852eb722473afffe165
Edward Williams <edwardwilliams@Edwards-MacBook-Pro.local>
2015-02-09 22:27:57 -0500

    sudo harder

--- capitalgain/startup.sh
+++ capitalgain/startup.sh
@@ -1,5 +1,3 @@
 export MONGO_URL='mongodb://cgains:moneysings@ds062097.mongolab.com:62097/capital_gain_database'
-export ROOT_URL='http://sonifythestockmarket.com'
-export PORT=80
 
-sudo -E meteor 
+sudo -E meteor --port 80
