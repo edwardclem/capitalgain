35ebd57a98526a04705cdcfc5be5d2feb6d54739
Edward Williams <edwardwilliams@Edwards-MacBook-Pro.local>
2015-02-09 21:48:29 -0500

    changed port

--- capitalgain/startup.sh
+++ capitalgain/startup.sh
@@ -1,3 +1,5 @@
 export MONGO_URL='mongodb://cgains:moneysings@ds062097.mongolab.com:62097/capital_gain_database'
+export ROOT_URL='http://sonifythestockmarket.com'
+export PORT=80
 
-meteor
+meteor 
