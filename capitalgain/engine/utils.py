74bb7ecc89674cef82e571be7a3c2da196323799
rmrao <Roshan@Roshans-MacBook-Pro.local>
2015-03-23 22:27:35 -0400

    changed get_variance to get_std

--- capitalgain/engine/utils.py
+++ capitalgain/engine/utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter1d
 
+""""""
+
 def read_data(filename, col=4):
     data = []
     with open(filename) as f:
@@ -27,7 +29,7 @@ def find_delta(data):
     delta = delta / top
     return delta
 
-def get_variance(data):
+def get_std(data):
     var = np.array([np.var(data[i:i+50]) for i in range(0, len(data), 5)])
     std = var**(0.5)
     std = gaussian_filter1d(std, 10)
@@ -38,7 +40,7 @@ def get_variance(data):
 def get_data(filename):
     data = read_data(filename)
     ma = get_ma(data, 20)
-    return ma[0::5], get_variance(ma)
+    return ma[0::5], get_std(ma)
 
 def get_delta(filename):
     data = read_data(filename)
