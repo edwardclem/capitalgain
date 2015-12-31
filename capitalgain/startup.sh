c3cd92082575781257d84a4f30d6c261eb3754db
Philip Mathieu <philip_mathieu@brown.edu>
2015-12-31 18:40:05 +0000

    Tweaks, debugs, etc.

--- capitalgain/startup.sh
+++ capitalgain/startup.sh
@@ -1,3 +1,3 @@
-export MONGO_URL="mongodb://capitalgain_db_2:.e7dLl9ZgD0tVVaUR5WOsVQQspcXumJ5xofqnEeA.I4-@ds056288.mongolab.com:56288/capitalgain_db_2"
+export MONGO_URL=mongodb://capitalgain_db_2:.e7dLl9ZgD0tVVaUR5WOsVQQspcXumJ5xofqnEeA.I4-@ds056288.mongolab.com:56288/capitalgain_db_2
 
-sudo -E meteor --port 80
+sudo -E meteor --port 8081
